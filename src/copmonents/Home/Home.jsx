7648d9d09cbff026bb175f52b4850fbf42dc139c
Ahmed-Mohamed-Yousry <ahmed11yousry@gmail.com>
2024-09-29 21:35:04 +0300

    edit style home over flow

--- src/copmonents/Home/Home.jsx
+++ src/copmonents/Home/Home.jsx
@@ -11,9 +11,9 @@ import CategoriesSlider from '../CategoriesSlider/CategoriesSlider';
 export default function Home() {
   return <>
 
+<div className='mx-auto overflow-hidden'>  
 
-
-  <div className='flex flex-col HomeStyle pt-32 pb-10 mt-7'>
+<div className='flex flex-col HomeStyle pt-32 pb-10 mt-7'>
   <img className='rounded-full w-82 h-60 mx-auto' src={HomePhoto} alt="" />
   <div className='my-5 text-white '>
   <h3 className='text-center text-5xl font-bold  dark:text-white py-5'>start Framework</h3>
@@ -27,9 +27,13 @@ export default function Home() {
   </div>
 
   </div>
-  
+
 <CategoriesSlider/>
 <Producte/>
+
+</div>
+
+  
   
   </>
 }
